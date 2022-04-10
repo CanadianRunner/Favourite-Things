78922be0e60842f53d30eeb8cf76f86cbe0d7333
Sean Keane <seankeane93@gmail.com>
2022-04-10 10:44:18 -0700

    added .val for 5 questions in JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
             $('#favorite-form').submit(function(event) {
                     event.preventDefault();
+                    const favoriteArray = [$('input#answer1').val(),
+                        $('input#answer2').val(),
+                        $('input#answer3').val(),
+                        $('input#answer4').val(),
+                        $('input#answer5').val()
+                    ];
+                    console.log(favoriteArray);
+
 
                 }
             }
