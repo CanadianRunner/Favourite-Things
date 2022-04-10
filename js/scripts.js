f2f42e5d304bb0196226ab1ef8020677dbc00690
Sean Keane <seankeane93@gmail.com>
2022-04-10 11:03:58 -0700

    finished pushing arrays and logging results

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-            $('#favorite-form').submit(function(event) {
-                    event.preventDefault();
-                    const favoriteArray = [$('input#answer1').val(),
-                        $('input#answer2').val(),
-                        $('input#answer3').val(),
-                        $('input#answer4').val(),
-                        $('input#answer5').val()
-                    ];
-                    console.log(favoriteArray);
+    $('#favorite-form').submit(function(event) {
+        event.preventDefault();
+        const favoriteArray = [$('input#answer1').val(),
+            $('input#answer2').val(),
+            $('input#answer3').val(),
+            $('input#answer4').val(),
+            $('input#answer5').val()
+        ];
+        console.log(favoriteArray);
 
+        console.log(favoriteArray[0], favoriteArray[1], favoriteArray[2]);
 
-                }
-            }
+        let newArray = [];
+        newArray.push(favoriteArray[0]);
+        newArray.push(favoriteArray[1]);
+        newArray.push(favoriteArray[2]);
+
+        console.log(newArray);
+
+        $('#result1').text(newArray[0]);
+        $('#result2').text(newArray[1]);
+        $('#result3').text(newArray[2]);
+    });
+});
